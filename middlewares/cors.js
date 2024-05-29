e616535077875f1f94e96f589db991ed458c1ad9
cybcrm <kipkomaria.13@gmail.com>
2024-05-29 22:42:43 +0300

    Дэзэшечка на проверку

---
+++ middlewares/cors.js
@@ -0,0 +1,22 @@
+app.use(
+    cors, 
+    bodyParser.json(),
+    express.static(path.join(__dirname, 'public')),
+    mainRoute,
+    gamesRouter
+)
+
+function cors(req, res, next) {
+    const { origin } = req.headers;
+    if (allowedCors.includes(origin)) { 
+        res.header('Access-Control-Allow-Origin', '*'); 
+    }
+    next();
+}
+
+const allowedCors = [
+    'https://practicum.yandex.ru',
+    'https://students-projects.ru',
+    'http://localhost:3000'
+  ];
+
